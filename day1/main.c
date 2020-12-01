d37a1d0733d03a8aee63629a55bff7ff126c7d61
gleneder <geder@purdue.edu>
2020-12-01 02:40:27 -0500

    Adds first implementation of solving part 1

--- day1/main.c
+++ day1/main.c
@@ -1,18 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "../utils/util.h"
+
+/*
+ *
+ * The goal of day 1 is to find the two values in the input 
+ * that sum to 2020 and calculate their product. 
+ *
+ *
+ *
+ */
 
 
 int main(int argc, char *argv[]) {
 
-	if(argc != 2) return -1;
+	//verify program params
+	if(argc != 2) {
+		printf("Ussage: <program> <input file>\n");	
+		return -1;
+	} 
 		
-	//call read input for testing right now
+	//handle reading input
 	readInput(argv[1]);
-
-	//verify that readInput works
-	struct input * curr = inputList;
+	
+	//Brute force this shit 
+	struct input * curr = inputList;		//get pointer to first input 
+	
+	//loop through inputs
 	while(curr) {
-		printf("Value stored: %s\n", curr->value);
+		int currValue = atoi(curr->value);	//get int rep of input value
+			
+		struct input * inputToAdd = curr->next;	//get pointer to next input in line
+		
+		//loop through rest of input to check for matches
+		while(inputToAdd) {
+			int toAdd = atoi(inputToAdd->value);
+			if( (currValue + toAdd) == 2020 ) {
+				printf("Key: %d\n", toAdd * currValue);
+				return 1;
+			}
+
+			//go to next node
+			inputToAdd = inputToAdd->next;
+		}
+
+		//go to next node 
 		curr = curr->next;
 	}
+
 }
