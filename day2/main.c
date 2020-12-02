bf411b7d155d4192df570257d6be5f86cb94728c
gleneder <geder@purdue.edu>
2020-12-02 01:23:45 -0500

    Adds description of problem in head of file

---
+++ day2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../utils/util.h"
+
+/*
+ * The goal of day 2 is to find all valid passwords 
+ * that follow the rule provided in each line.
+ * Given input "1-3 a: abcde", there must contain at 
+ * least 1 instance 'a' and at most 3 instances of 'a'
+ * in the following passcode. Which in the provided input 
+ * will pass. 
+ */
+int main(int argc, char *argv[]) {
+
+
+	
+}
