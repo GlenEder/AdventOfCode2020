b1f435dacddea8eb705d5b19f1fd2a686aadfdb5
gleneder <geder@purdue.edu>
2020-12-05 01:47:04 -0500

    Lamo, I already had a template file

---
+++ utils/startFile.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "../utils/util.h"
+
+
+int main(int argc, char *argv[]) {
+
+	//check program args
+	if(argc != 2) {
+		printf("Ussage: <program> <input file>\n");
+		return -1;
+	}	
+
+	//read the input file
+	readInput(argv[1]);
+
+}
