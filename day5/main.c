caf0cf461ab913554893d2834105fbd641ff1797
gleneder <geder@purdue.edu>
2020-12-05 01:51:53 -0500

    Adds part 1 description

--- day5/main.c
+++ day5/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include "../utils/util.h"
 
+/*
+ * Day 5 is about those dang bording passes. 
+ * The use binary space partitioning to represent 
+ * where the seat of the ticket is. There are 
+ * a total of 128 rows and 8 columns. Our goal
+ * for part 1 is to find the seat, and use its 
+ * seat position to calculate the highest id.
+ * 
+ * Seat Id = Row * 8 + Column
+ *
+ */
 
 int main(int argc, char *argv[]) {
 
