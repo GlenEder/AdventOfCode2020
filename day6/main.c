8662e124a75d3b860279f54656a1cc4e69cc9065
gleneder <geder@purdue.edu>
2020-12-06 00:42:53 -0500

    Adds starting file to day6 dir

---
+++ day6/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "../utils/util.h"
+
+
+void part1() {
+
+}
+
+void part2() {
+
+}
+
+int main(int argc, char *argv[]) {
+
+	//check program args
+	if(argc != 2) {
+		printf("Ussage: <program> <input file>\n");
+		return -1;
+	}	
+
+	//read the input file
+	readInput(argv[1]);
+	
+	part1();
+	part2();	
+
+	//free memory in input list
+	cleanup();
+}
