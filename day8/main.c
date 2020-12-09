4f6de7ef9b5fa913b7f96b023ee2afea693c3a9e
gleneder <geder@purdue.edu>
2020-12-09 02:45:12 -0500

    Prints to add substring

--- day8/main.c
+++ day8/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "../utils/util.h"
 
 /*
@@ -12,6 +14,26 @@
 
 
 void part1() {
+
+	struct input * curr = inputList;		//get pointer to first command
+	int accumulator = 0;				//accumulator tracker 
+	int looped = 0;					//boolean for loop
+	
+	while(!looped) {
+
+		//check for nop command
+		if(strstr(curr->value, "nop")) { curr = curr->next; }	
+	
+		if(strstr(curr->value, "acc")) {
+			int l = strlength(curr->value);					//get length of string
+			int offset = 5;							//offset to go over opp code
+			char * toAdd = substring(curr->value, offset, l-offset); 	//get substring for add/sub
+			printf("ToAdd: %s\n", toAdd);	
+			free(toAdd);
+		}
+
+		looped = 1;
+	}
 
 }
 
