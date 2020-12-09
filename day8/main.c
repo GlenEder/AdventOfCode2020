1597751e14dd1b7e3901468377bc9cab40876440
gleneder <geder@purdue.edu>
2020-12-09 03:07:20 -0500

    Adds base case and adding to accumulator

--- day8/main.c
+++ day8/main.c
@@ -17,23 +17,47 @@ void part1() {
 
 	struct input * curr = inputList;		//get pointer to first command
 	int accumulator = 0;				//accumulator tracker 
-	int looped = 0;					//boolean for loop
 	
-	while(!looped) {
+	while(1) {
+
+		//check if visited 
+		if(curr->visited) break;
+		
+		//set visited value for current node 
+		curr->visited = 1;
 
 		//check for nop command
-		if(strstr(curr->value, "nop")) { curr = curr->next; }	
-	
-		if(strstr(curr->value, "acc")) {
-			int l = strlength(curr->value);					//get length of string
-			int offset = 5;							//offset to go over opp code
-			char * toAdd = substring(curr->value, offset, l-offset); 	//get substring for add/sub
-			printf("ToAdd: %s\n", toAdd);	
-			free(toAdd);
+		if(strstr(curr->value, "nop")) { 
+			printf("here\n"); 
+			//curr = curr->next; 
+			continue;
 		}
 
-		looped = 1;
+		printf("Also here\n");
+
+		int jumping = 0;						//jumping flag	
+		if(strstr(curr->value, "jmp")) { jumping = 1; }			//set flag for jump instruction 
+
+		int adding = 0;							//add/sub flag
+		if( *(curr->value + 4) == '+' ) { adding = 1; }			//set adding flag
+
+		int l = strlength(curr->value);					//get length of string
+		int offset = 5;							//offset to go over opp code
+		char * toAdd = substring(curr->value, offset, l-offset); 	//get substring for add/sub
+		offset  = atoi(toAdd);						//get int value of add/sub value 
+			
+		if(jumping) {
+				
+		}
+		else {
+			accumulator = adding ? accumulator + offset : accumulator - offset;		//add or subtract value to accumlator 
+			curr = curr->next;								//go to next instruction 
+		}
+
+		free(toAdd);
 	}
+
+	printf("Accumulator: %d\n", accumulator);
 
 }
 
