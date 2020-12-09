3dd2d711a4bc70a2d9dc7b78ea6d2468453f2442
gleneder <geder@purdue.edu>
2020-12-09 15:03:15 -0500

    Adds start of implementation, likey to change as I want a queue

--- day9/main.c
+++ day9/main.c
@@ -2,6 +2,42 @@
 #include <stdlib.h>
 #include "../utils/util.h"
 
+/*
+ * It's day 9, and that means we're looking
+ * into XMAS. That is eXchange-Masking 
+ * Addition System. The system puts 25 
+ * values at the beginning to start the 
+ * encryption. From there, the next number 
+ * must be a sum of two of the previous 25 
+ * values. For part 1, we are to find the fisrt
+ * number that does not follow this rule in 
+ * our input. 
+ */
+
+
+
+//Given the size of the preable provided, finds the frist wrong
+//value in the input file
+//@param preamble -- size of preamble buffer
+void findWrongSum(int preamble) {
+
+	int sumOptions[preamble] = {0};		//create array for checking sums
+	struct input * curr = inputList;	//get pointer to first input line
+
+	//fill preamble data 
+	for(int i = 0; i < preamble; i++) {
+		sumOptions[i] = atoi(curr->value);	//convert input to int
+		curr = curr->next;			//go to next node
+	}
+
+	//loop through input, find sum error
+	while(curr) {
+		int sumToFind = atoi(curr->value);	//get int value of next input 
+		
+	}
+
+}
+
 
 void part1() {
 
