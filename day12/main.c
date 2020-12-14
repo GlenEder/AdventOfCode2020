cb285ebf8b5b8deab924d660d79ec6b16d68c8aa
gleneder <geder@purdue.edu>
2020-12-14 05:34:31 -0500

    Finishes day 12

--- day12/main.c
+++ day12/main.c
@@ -66,6 +66,79 @@ void part1() {
 
 void part2() {
 
+	int waypointEW = 10;		//starting east west cord of waypoint
+	int waypointNS = 1;		//starting north south cord of waypoint
+
+	int xPos = 0;
+	int yPos = 0;
+	
+	struct input * curr = inputList;	//get pointer to first input 
+	while(curr) {
+	
+		char c = *curr->value;								//get direction char
+		char * amountString = substring(curr->value, 1, strlength(curr->value) - 1);	//get value substring
+		int  amount = atoi(amountString);						//get int representation 
+		free(amountString);								//free substring memory 
+		
+		/* Handle Directions */
+		switch(c) {					
+			case 'N': 				
+				waypointNS += amount;	
+				break;
+			case 'S':
+				waypointNS -= amount;
+				break;
+			case 'E': 
+				waypointEW += amount;
+				break;
+			case 'W': 
+				waypointEW -= amount;
+				break;
+			case 'R':
+				if(amount == 90) {
+					int oldNS = waypointNS;
+					waypointNS = -waypointEW;	
+					waypointEW = oldNS;
+				}
+				else if(amount == 180) {
+					waypointNS *= -1;
+					waypointEW *= -1;
+				}	
+				else if(amount == 270) {
+					int oldNS = waypointNS;
+					waypointNS = waypointEW;
+					waypointEW = -oldNS;
+				}
+				break;
+			case 'L':
+				if(amount == 270) {
+					int oldNS = waypointNS;
+					waypointNS = -waypointEW;	
+					waypointEW = oldNS;
+				}
+				else if(amount == 180) {
+					waypointNS *= -1;
+					waypointEW *= -1;
+				}	
+				else if(amount == 90) {
+					int oldNS = waypointNS;
+					waypointNS = waypointEW;
+					waypointEW = -oldNS;
+				}
+				break;
+			case 'F':
+				xPos += waypointEW * amount;
+				yPos += waypointNS * amount;
+		}
+	
+		//printf("Dir: %c, Amt: %d, New Heading: %d, Updated Cords: (%d, %d)\n", c, amount, heading, eastWest, northSouth);	
+	
+		curr = curr->next;				//go to next input node
+	} 
+
+	//print results
+	printf("Manhattan Distance: %d\n", abs(xPos) + abs(yPos));	
+
 }
 
 int main(int argc, char *argv[]) {
