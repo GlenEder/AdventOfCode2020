1453b9406a8111a1ddfb75a78159e18f3d8b3429
gleneder <geder@purdue.edu>
2020-12-13 21:46:40 -0500

    Adds test input file

--- day11/main.c
+++ day11/main.c
@@ -2,8 +2,27 @@
 #include <stdlib.h>
 #include "../utils/util.h"
 
+/*
+ * Day 11 is musical chairs; well not really. 
+ * We are to find the number of empty seats 
+ * after the given rules result in no change 
+ * of seating arragement. 
+ */
+
+#define FLOOR 0
+#define FREE 1
+#define TAKEN 2
 
 void part1() {
+
+	int WIDTH  = strlength(inputList->value);	//get width of grid
+	int HEIGHT = 0;					//height of grid
+	struct input * curr = inputList;		//pointer to beginning of input list
+	while(curr) { HEIGHT++; curr = curr->next; }	//find height of grid
+
+	//initalize map	
+	int map[WIDTH][HEIGHT];
+
 
 }
 
