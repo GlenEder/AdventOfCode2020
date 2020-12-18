448d549cd21a322fe831900d99e5773480b53653
gleneder <geder@purdue.edu>
2020-12-17 19:43:36 -0500

    Time to restart part 2

--- day14/main.c
+++ day14/main.c
@@ -21,7 +21,10 @@
  * the next pointer to null when creating
  * new nodes.
  * 
- * 
+ * Part 2 I read wrong so this is going to 
+ * take a bit longer than I want it to. The 
+ * value is set to all the memory addresses 
+ * that the mask emits.  
  */
 
 struct MemoryData {
@@ -200,134 +203,8 @@ char * applyMaskV2(char * mask, int value) {
 	
 }
 
-//Recursive method to get the value of the provided string value
-//@param value -- bit string to eval 
-//@param power -- current power to eval 
-//
-//@return sum of string 
-unsigned long getValue(char * value, int power, int numXs) {
-
-	
-	char c = *value;
-	int xOptions = pow(2, numXs);
-
-	printf("C: %c, Pow: %d, Num X's: %d\n", c, power, numXs);
-	if(power == 0) {
-		if(c == 'X') return xOptions / 2;
-		if(c == '1') return 1 * xOptions;
-		return 0;
-	}
-
-	if(c == 'X') {
-		return pow(2, power) * xOptions / 2 + getValue(value + 1, power - 1, numXs);
-	}
-	else if (c == '1') {
-		return pow(2, power) * xOptions + getValue(value + 1, power - 1, numXs);
-	}
-	else if (c == '0') {
-		return getValue(value + 1, power - 1, numXs);
-	}
-
-	printf("How did i get here...\n");	
-	return 0;
-}
-
-//Calculates the sum of the values in the memory list
-//@return sum of memory list
-unsigned long getSumV2() {
-	
-	unsigned long sum = 0;
-	struct MemoryData * curr = firstMemoryData;
-	while(curr) {
-		
-		char * pos = curr->value;
-		int numXs = 0;
-		while(*pos != 0) { 
-   			if(*pos == 'X') numXs++; 	
-			pos++; 
-    		}
-		unsigned long toAdd = getValue(curr->value, 35, numXs);
-		printf("Adding %lu\n", toAdd);
-		sum += toAdd;
-		curr = curr->next;
-	}
-
-	return sum;
-
-}
 void part2() {
 
-	struct input * curr = inputList;			//get pointer to first input node
-	char * currentMask = malloc(sizeof(char) * 38);		//pointer to current mask 
-	while(curr) {
-			
-		/* handle mask input */
-		if(strstr(curr->value, "mask")) {									//check for mask input 
-			int indexOfMask = indexOfChar(curr->value, '=', 0) + 2; 					//get index of start of substring
-			char * newMask = substring(curr->value, indexOfMask, strlength(curr->value) - indexOfMask - 1);	//get mask substring
-			memcpy(currentMask, newMask, strlength(newMask) + 1); 						//copy new mask into masks memory 
-			free(newMask);
-		} 
-
-		/* handle memory input */
-		else {
-			int frontOfMem = indexOfChar(curr->value, '[', 0) + 1;				//get index of memory address
-			int endOfMem = indexOfChar(curr->value, ']', frontOfMem);			//get index of end of memory address
-			char * memAddy = substring(curr->value, frontOfMem, endOfMem - frontOfMem);	//get substring of memory address
-			int memAddress = atoi(memAddy);							//get in rep of memory address		
-			free(memAddy);									//free substring memory 
-			
-			int indexOfValue = indexOfChar(curr->value, '=', endOfMem) + 1; 		//get index of start of substring
-			int l = strlength(curr->value) - 1;
-			char * valueString = substring(curr->value, indexOfValue, l - indexOfValue);	//get substring
-			int value = atoi(valueString);							//get int rep of value
-			free(valueString);								//free substring memory 
-			
-			char * newValue = applyMaskV2(currentMask, memAddress);				//apply mask to value provided
-			
-			/* Handle adding to list/updating node */
-			struct MemoryData * currMem = firstMemoryData;					//get pointer to head of list
-			int updatedFlag = 0;								//flag for if memory was updated or not
-			while(currMem) {
-				if(currMem->key == memAddress) {		
-					memcpy(currMem->value, newValue, strlength(newValue) + 1);	//update value stored 
-					free(newValue);							//free new value memory 
-					updatedFlag++;							//set updated flag
-					break;
-				}
-				
-				currMem = currMem->next;						//go to next memory node
-			}				
-
-			if(!updatedFlag) {
-				struct MemoryData * newData = malloc(sizeof(struct MemoryData));	//create memory for new node
-				newData->key = memAddress;						//set key
-				newData->next = NULL;							//set next to null
-				newData->value = malloc(sizeof(char) * (strlength(newValue) + 1));	//create memory for value string
-				memcpy(newData->value, newValue, strlength(newValue) + 1);		//copy value info 
-				free(newValue);								//free newValue memory				
-
-				if(firstMemoryData) {
-					currMem = firstMemoryData;					//set to head node
-					while(currMem->next) { currMem = currMem->next; }		//go to end of list
-					currMem->next = newData;					//add to end
-				}
-				else {
-					firstMemoryData = newData;					//init head node
-				}
-			}
-		}	
-		curr = curr->next;			//go to next input 
-	}
-
-/*	//print memory list at end for debugging
-	struct MemoryData * currMem = firstMemoryData;
-	while(currMem) {
-		printf("memAddress: %d, value: %s\n", currMem->key, currMem->value);
-		currMem = currMem->next;
-	}
-*/
-	printf("Total Sum: %lu\n", getSumV2());
 }
 
 int main(int argc, char *argv[]) {
