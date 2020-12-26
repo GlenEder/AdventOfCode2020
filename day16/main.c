24cac0b8f13133f2c3a5ea97dfdab6eb6045f206
gleneder <geder@purdue.edu>
2020-12-26 15:31:04 -0500

    Adds all ranges to range list

--- day16/main.c
+++ day16/main.c
@@ -11,34 +11,61 @@
  * the invalid numbers in the tickets.
  */
 
+
+struct node * addRange(char * inputSegment, struct node * firstRange) {
+
+	int fNumIndex = indexOfNumber(inputSegment, 0);							//get index of first number in string
+	int fDiv = indexOfChar(inputSegment, '-', fNumIndex);					//get index of first '-'
+	char * fNumString = positionsSubstring(inputSegment, fNumIndex, fDiv);	//get substring of lower value
+	int firstLower = atoi(fNumString);										//int rep of substring value
+	free(fNumString);														//free substring memory
+
+	int sNumIndex = indexOfNumber(inputSegment, fDiv);						//get index of second number
+	int sEnd = indexOfChar(inputSegment, ' ', sNumIndex);					//get index of space
+	if(sEnd < 0) {
+		sEnd = strlength(inputSegment);										//if no space, get index of end of string
+	}
+
+	char * sNumString = positionsSubstring(inputSegment, sNumIndex, sEnd);	//get second number substring
+	int secLower = atoi(sNumString);										//convert to int
+	free(sNumString);														//free substring memory
+
+	/* Add range to ranges list */
+	if(firstRange) {
+		addNewKeyedNode(firstRange, &firstLower, &secLower, sizeof(int), sizeof(int));
+	}
+		/* Create first range node */
+	else {
+		/* Make key the lower value of the range, and the value the upper value of the range */
+		firstRange = createKeyedList(&firstLower, &secLower, sizeof(int), sizeof(int));
+	}
+
+	return firstRange;
+}
+
 void part1() {
 
 	struct input * curr = inputList;
+	struct node * firstRange = NULL;
 
 	/* get ranges from input file */
 	while(strcompare(curr->value, "\n")) {
-
-		int fNumIndex = indexOfNumber(curr->value, 0);							//get index of first number in string
-		int fDiv = indexOfChar(curr->value, '-', fNumIndex);					//get index of first '-'
-		char * fNumString = positionsSubstring(curr->value, fNumIndex, fDiv);	//get substring of lower value
-
-		int firstLower = atoi(fNumString);										//int rep of substring value
-		free(fNumString);														//free substring memory
-
-		int sNumIndex = indexOfNumber(curr->value, fDiv);
-		int sEnd = indexOfChar(curr->value, ' ', sNumIndex);
-		char * sNumString = positionsSubstring(curr->value, sNumIndex, sEnd);
-
-		int secLower = atoi(sNumString);
-		free(sNumString);
-
-
-		printf("Numbers %d - %d\n", firstLower, secLower);
-
-
-		curr = curr->next;														//go to next input node
+		firstRange = addRange(curr->value, firstRange);												//add first range
+		int backIndex = indexOfChar(curr->value, 'o', indexOfNumber(curr->value, 0));				//get index of o in or
+		char * secondRange = positionsSubstring(curr->value, backIndex, strlength(curr->value));	//get substring of second range
+		firstRange = addRange(secondRange, firstRange);												//add second range to list
+		curr = curr->next;																			//go to next input node
 	}
 
+	/* print list for debugging */
+	struct node * currRange = firstRange;
+	while(currRange) {
+		printf("Range: %d - %d\n", *(int *)currRange->key, *(int *)currRange->value);
+		currRange = currRange->next;
+	}
+
+
+	deleteList(firstRange);																			//free list memory
 }
 
 void part2() {
