bf91ddb7b6d28b6260bd77ea921bc1ae32461420
gleneder <geder@purdue.edu>
2021-02-05 17:50:58 -0500

    Updates adding ranges to mesh with part1

--- day16/main.c
+++ day16/main.c
@@ -12,7 +12,7 @@
  * the invalid numbers in the tickets.
  */
 
-
+/* Ticket Section code was made during Part 2*/
 struct ticketSection {
 	int id;							//line number of section in input
 	int validPositions;				//using positions 0-19 for setting valid positions
@@ -23,13 +23,56 @@ struct ticketSection {
 
 struct ticketSection * firstTicketSection = NULL;
 
-void addTicketSection(int id, int lRange[2], int uRange[2]) {
 
+/*
+ * Adds data to ticket section nodes
+ * @param id -- ticket section id
+ * @param low -- lower value in range to add
+ * @param high -- upper value in range to add
+ * @param isUpperRange -- if is the upper range of the two ranges
+ */
+void addTicketSectionData(int id, int low, int high, int isUpperRange) {
+
+	//check to see if node exists already
+	struct ticketSection * currSection = firstTicketSection;
+	while(currSection) {
+
+		//compare id's
+		if(currSection->id == id) {
+
+			//add range
+			if(isUpperRange) {
+				currSection->upperRange[0] = low;
+				currSection->upperRange[1] = high;
+			}
+			else {
+				currSection->lowerRange[0] = low;
+				currSection->lowerRange[1] = high;
+			}
+
+			//exit function
+			return;
+		}
+
+		//go to next section
+		currSection = currSection->next;
+	}
+
+
+
+	//If section doesn't exist add to list
 	struct ticketSection * newSection = (struct ticketSection *)malloc(sizeof(struct ticketSection));
 	newSection->id = id;
 	newSection->validPositions = 1048575;	//decimal equal of 20 1's in binary
-	memcpy(newSection->lowerRange, lRange, sizeof(int) * 2);
-	memcpy(newSection->upperRange, uRange, sizeof(int) * 2);
+	//add range
+	if(isUpperRange) {
+		newSection->upperRange[0] = low;
+		newSection->upperRange[1] = high;
+	}
+	else {
+		newSection->lowerRange[0] = low;
+		newSection->lowerRange[1] = high;
+	}
 
 	//assign first ticket
 	if(firstTicketSection == NULL) {
@@ -41,8 +84,6 @@ void addTicketSection(int id, int lRange[2], int uRange[2]) {
 		while(currSection->next != NULL) { currSection = currSection->next; }
 		currSection->next = newSection;
 	}
-
-
 }
 
 //Checks if the number is within a range
@@ -65,9 +106,10 @@ int isValidNumber(int number, struct node * firstRange) {
 //Adds range to ranges list given the input string
 //@param inputSegment -- string to find range in
 //@param firstRange -- pointer to head of range list
+//@param sectionId -- id of section to add data to			/* Part 2 addition */
 //
 //@return pointer to head of updated range list
-struct node * addRange(char * inputSegment, struct node * firstRange) {
+struct node * addRange(char * inputSegment, struct node * firstRange, int sectionId) {
 
 	int fNumIndex = indexOfNumber(inputSegment, 0);							//get index of first number in string
 	int fDiv = indexOfChar(inputSegment, '-', fNumIndex);					//get index of first '-'
@@ -102,14 +144,16 @@ void part1() {
 
 	struct input * curr = inputList;
 	struct node * firstRange = NULL;
+	int sectionCount = 0;
 
 	/* get ranges from input file */
 	while(strcompare(curr->value, "\n")) {
-		firstRange = addRange(curr->value, firstRange);												//add first range
+		firstRange = addRange(curr->value, firstRange, sectionCount);								//add first range
 		int backIndex = indexOfChar(curr->value, 'o', indexOfNumber(curr->value, 0));				//get index of o in or
 		char * secondRange = positionsSubstring(curr->value, backIndex, strlength(curr->value));	//get substring of second range
-		firstRange = addRange(secondRange, firstRange);												//add second range to list
+		firstRange = addRange(secondRange, firstRange, sectionCount);								//add second range to list
 		curr = curr->next;																			//go to next input node
+		sectionCount++;																				//increment section id counter
 	}
 
 	//print list for debugging
