c5be050895a470285c4b845f20851c03e6bcc0e8
gleneder <geder@purdue.edu>
2021-02-07 08:30:19 -0500

    Adds input file, and starting c file

---
+++ day17/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../utils/util.h"
+
+/*
+ * It's day 17 and after my long break
+ * from this challenge I am back (I admit
+ * that I tried to finish day 16 part 2
+ * last night but that will be done another
+ * day). Today's challenge has us simulate
+ * six cycles of this boot cycle.
+ */
+
+
+
+void part1() {
+
+
+
+}
+
+void part2() {
+
+}
+
+int main(int argc, char *argv[]) {
+
+	//check program args
+	if(argc != 2) {
+		printf("Ussage: <program> <input file>\n");
+		return -1;
+	}	
+
+	//read the input file
+	readInput(argv[1]);
+	
+	part1();
+	part2();	
+
+	//free memory in input list
+	cleanup();
+}
